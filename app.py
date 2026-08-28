6d63604f7e127064ee153a15091f841a2bd79944
steve8413 <stavyashah5@gmail.com>
2026-08-28 18:32:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -237,7 +237,7 @@ if os.path.exists("logo.png"):
         encoded_logo = base64.b64encode(img_file.read()).decode()
     st.markdown(f"""
         <div style="margin-bottom: 2px;">
-            <img src="data:image/png;base64,{encoded_logo}" style="width: 350px; height: auto;" />
+            <img src="data:image/png;base64,{encoded_logo}" style="width: 370px; height: auto;" />
         </div>
     """, unsafe_allow_html=True)
 
